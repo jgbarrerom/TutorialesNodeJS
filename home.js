52f226b15028ce457da16b683f3ef6707a949a3e
Jeisson Barrero <jgb_17@hotmail.com>
2019-01-27 23:07:05 -0500

    Se agregan ajustes para el index y se agrega el archivo app.js que usa libreria express que se instalo con npm

--- home.js
+++ home.js
@@ -5,10 +5,16 @@ const port = '8080';
 //leer archivs con file system
 //var html = fs.readFileSync("./index.html");
 
+
+
 http.createServer((req,res) => {
-  var html = fs.readFileSync("./index.html");
-  res.write(html);
-  res.end();
+  console.log(req.url);
+  var html = fs.readFile("./index.html",(err,html) => {
+    res.writeHead(200,{'Content-Type':'text/html'});
+    res.write(html);
+    res.end();
+  });
+
 }).listen(port,hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
